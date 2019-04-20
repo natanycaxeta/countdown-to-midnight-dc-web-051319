887b91913095dc42df89c65e76ccc5e05b697ce8
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-20 23:53:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
+ 
 
-def countdown
+def countdown.sleep(1)
+   countdown = 5 
+  while countdown > 1 
+  puts "#{countdown} SECOND(S)!"
+  countdown -= 1
+  "HAPPY NEW YEAR!"
+end
 end
